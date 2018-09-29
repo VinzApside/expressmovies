0d0194f6e8686e508206fca0e12023768a03fa46
Vinz <vinzapside@gmail.com>
2018-09-29 18:21:12 +0200

    ajout de route et de ejs

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const app = express();
 
 const port = 3000;
 
+app.get("/movies", (req, res) => {
+  res.send("bientôt une liste de film ici !");
+});
+
+app.get("/movies/add", (req, res) => {
+  res.send("route add");
+});
+
+app.get("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`vous regardez le film numéro ${id}`);
+});
+
 app.get("/", (req, res) => {
   res.send("hello world !");
 });
